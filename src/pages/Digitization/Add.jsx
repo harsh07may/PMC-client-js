50d9286eeed43d9e278000ea2554754349aac461
Harsh Naik <harsh07may@gmail.com>
2023-03-17 12:02:39 +0530

    Added Login page and styling

--- src/pages/Digitization/Add.jsx
+++ src/pages/Digitization/Add.jsx
@@ -4,6 +4,9 @@ import MuncipalProperty from "../../components/Digitization/forms/MuncipalProper
 import HouseTax from "../../components/Digitization/forms/HouseTax";
 import ConLisc from "../../components/Digitization/forms/ConstructionLicense";
 import BirthRecords from "../../components/Digitization/forms/BirthRecords";
+import "./Add.css";
+import { Layout } from "antd";
+const { Content, Header, Sider } = Layout;
 
 const Add = () => {
   const [documentType, setDocumentType] = useState("Properties");
@@ -16,65 +19,51 @@ const Add = () => {
   };
 
   return (
-    <div className="flex flex-row mt-10">
-      <div className="flex flex-col w-w-sweet items-center">
-        <button
-          key="1"
-          onClick={(e) => handleClick(e, 1)}
-          value="Properties"
-          //   className="bg-slate-400 rounded-lg m-2 p-6"
-          className={
-            selectedButtonId == "1"
-              ? "bg-blue-300 rounded-lg m-2 h-20 w-48 text-white font-bold text-left pl-4"
-              : "bg-blue-500 rounded-lg m-2 h-20 w-48 text-white font-bold text-left pl-4"
-          }
-        >
-          MUNCIPALITY PROPERTY RECORDS
-        </button>
-        <button
-          key="2"
-          onClick={(e) => handleClick(e, 2)}
-          value="HouseTax"
-          className={
-            selectedButtonId == "2"
-              ? "bg-blue-300 rounded-lg m-2 h-20 w-48 text-white font-bold text-left pl-4"
-              : "bg-blue-500 rounded-lg m-2 h-20 w-48 text-white font-bold text-left pl-4"
-          }
-        >
-          HOUSE TAX RECORDS
-        </button>
-        <button
-          key="3"
-          onClick={(e) => handleClick(e, 3)}
-          value="ConLisc"
-          className={
-            selectedButtonId == "3"
-              ? "bg-blue-300 rounded-lg m-2 w-48 h-20 text-white font-bold text-left pl-4"
-              : "bg-blue-500 rounded-lg m-2 w-48 h-20 text-white font-bold text-left pl-4"
-          }
-        >
-          CONSTRUCTION LICENSES
-        </button>
-        <button
-          key="4"
-          onClick={(e) => handleClick(e, 4)}
-          value="BirthRecords"
-          className={
-            selectedButtonId == "4"
-              ? "bg-blue-300 rounded-lg m-2 w-48 h-20 text-white font-bold text-left pl-4"
-              : "bg-blue-500 rounded-lg m-2 w-48 h-20 text-white font-bold text-left pl-4"
-          }
-        >
-          BIRTH RECORDS
-        </button>
-      </div>
-      <div className="w-full m-5">
-        {documentType == "Properties" && <MuncipalProperty />}
-        {documentType == "HouseTax" && <HouseTax />}
-        {documentType == "ConLisc" && <ConLisc />}
-        {documentType == "BirthRecords" && <BirthRecords />}
-      </div>
-    </div>
+    <>
+      <Layout>
+        <Sider theme="light">
+          <button
+            key="1"
+            onClick={(e) => handleClick(e, 1)}
+            value="Properties"
+            //   className="bg-slate-400 rounded-lg m-2 p-6"
+            className={selectedButtonId == "1" ? "btn" : "btn btn-selected"}
+          >
+            MUNCIPALITY PROPERTY RECORDS
+          </button>
+          <button
+            key="2"
+            onClick={(e) => handleClick(e, 2)}
+            value="HouseTax"
+            className={selectedButtonId == "2" ? "btn" : "btn btn-selected"}
+          >
+            HOUSE TAX RECORDS
+          </button>
+          <button
+            key="3"
+            onClick={(e) => handleClick(e, 3)}
+            value="ConLisc"
+            className={selectedButtonId == "3" ? "btn" : "btn btn-selected"}
+          >
+            CONSTRUCTION LICENSES
+          </button>
+          <button
+            key="4"
+            onClick={(e) => handleClick(e, 4)}
+            value="BirthRecords"
+            className={selectedButtonId == "4" ? "btn" : "btn btn-selected"}
+          >
+            BIRTH RECORDS
+          </button>
+        </Sider>
+        <Content>
+          {documentType == "Properties" && <MuncipalProperty />}
+          {documentType == "HouseTax" && <HouseTax />}
+          {documentType == "ConLisc" && <ConLisc />}
+          {documentType == "BirthRecords" && <BirthRecords />}
+        </Content>
+      </Layout>
+    </>
   );
 };
 
