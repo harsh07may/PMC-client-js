1794846f178d1e8e2593e0f3e8026c8e58067ad3
Nevan Alphonso <nevanalphonso@gmail.com>
2023-04-16 20:35:22 +0530

    made app Gallery responsive

--- src/pages/AppGallery/AppGallery.jsx
+++ src/pages/AppGallery/AppGallery.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PoweroffOutlined } from "@ant-design/icons";
-import { Card, Button } from "antd";
+import { Card, Button, Row, Col } from "antd";
 const { Meta } = Card;
 
 import { NavLink, Outlet, useNavigate, redirect } from "react-router-dom";
@@ -33,50 +33,65 @@ export default function AppGallery() {
         >
           Logout
         </Button>
-        <div className={styles.heading} style={{ color: "white" }}>
+        <div className={styles.heading}>
           <h1>Select a Service</h1>
         </div>
 
         <div className={styles.gallery}>
-          <Card
-            className={styles.expandCard}
-            onClick={() => navigate("/digitization")}
-            hoverable
-            style={{ width: 220 }}
-            cover={<img src={documentLogo} style={{ height: 200 }} />}
-          >
-            <Meta title="File Digitization" description="Digitize your files" />
-          </Card>
-          <Card
-            className={styles.expandCard}
-            onClick={() => console.log("2")}
-            hoverable
-            style={{ width: 220 }}
-            cover={<img src={calenderLogo} style={{ height: 200 }} />}
-          >
-            <Meta title="Leave Management" description="Manage your leaves" />
-          </Card>
-          <Card
-            className={styles.expandCard}
-            onClick={() => console.log("3")}
-            hoverable
-            style={{ width: 220 }}
-            cover={<img src={compassLogo} style={{ height: 200 }} />}
-          >
-            <Meta
-              title="File Tracking"
-              description="Track your files in-office"
-            />
-          </Card>
-          <Card
-            onClick={() => console.log("4")}
-            className={styles.expandCard}
-            hoverable
-            style={{ width: 220 }}
-            cover={<img src={userLogo} style={{ height: 200 }} />}
-          >
-            <Meta title="User Managemnt" description="Manage user accounts" />
-          </Card>
+          <Row className={styles.antRow} gutter={[50, 50]}>
+            <Col xs={24} lg={{ span: 12, order: 4 }} xl={{ span: 6 }}>
+              <Card
+                className={styles.expandCard}
+                onClick={() => navigate("/digitization")}
+                hoverable
+                cover={<img src={documentLogo} className={styles.cardLogo} />}
+              >
+                <Meta
+                  title="File Digitization"
+                  description="Digitize your files"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} lg={{ span: 12, order: 4 }} xl={{ span: 6 }}>
+              <Card
+                className={styles.expandCard}
+                onClick={() => console.log("2")}
+                hoverable
+                cover={<img src={calenderLogo} className={styles.cardLogo} />}
+              >
+                <Meta
+                  title="Leave Management"
+                  description="Manage your leaves"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} lg={{ span: 12, order: 4 }} xl={{ span: 6 }}>
+              <Card
+                className={styles.expandCard}
+                onClick={() => console.log("3")}
+                hoverable
+                cover={<img src={compassLogo} className={styles.cardLogo} />}
+              >
+                <Meta
+                  title="File Tracking"
+                  description="Track your files in-office"
+                />
+              </Card>
+            </Col>
+            <Col xs={24} lg={{ span: 12, order: 4 }} xl={{ span: 6 }}>
+              <Card
+                onClick={() => console.log("4")}
+                className={styles.expandCard}
+                hoverable
+                cover={<img src={userLogo} className={styles.cardLogo} />}
+              >
+                <Meta
+                  title="User Management"
+                  description="Manage user accounts"
+                />
+              </Card>
+            </Col>
+          </Row>
         </div>
       </div>
     </>
--- src/pages/Digitization/AdminDashboard/AdminDashboard.jsx
+++ src/pages/Digitization/AdminDashboard/AdminDashboard.jsx
@@ -2,78 +2,70 @@ import React, { useState } from "react";
 import { Form, Input, Button, Row, Col } from "antd";
 const { Item } = Form;
 
-// const layout = {
-//   labelCol: { span: 8 },
-//   wrapperCol: { span: 16 },
-// };
-
-// const tailLayout = {
-//   wrapperCol: { offset: 8, span: 16 },
-// };
-
-const layout = {
-  labelCol: {
-    // xs: { span: 24 },
-    // sm: { span: 12 },
-    // md: { span: 8 },
-    // lg: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 2 },
-    // sm: { span: 12 },
-    // md: { span: 14 },
-    // lg: { span: 16 },
-  },
-};
-const tailLayout = {
-  wrapperCol: {
-    xs: { offset: 12 },
-    sm: { span: 1, offset: 12 },
-    // md: { span: 1, offset: 8 },
-    md: { span: 1 },
-    // lg: { span: 12, offset: 8 },
-  },
-};
-
 const AdminDashboard = () => {
   return (
-    // <Form {...layout}>
-    //   <Item label="Name" name="username" style={{ backgroundColor: "blue" }}>
-    //     {" "}
-    //     <Input type="text" />{" "}
-    //   </Item>
-    //   <Item
-    //     label="Password"
-    //     name="password"
-    //     style={{ backgroundColor: "blue" }}
-    //   >
-    //     {" "}
-    //     <Input type="password" />{" "}
-    //   </Item>
-    //   <Item {...tailLayout} style={{ backgroundColor: "blue" }}>
-    //     <Button htmlType="submit" type="primary">
-    //       {" "}
-    //       Submit{" "}
-    //     </Button>
-    //   </Item>
-    // </Form>
-    <Row align="middle" justify="center">
-      <Col xs={24} sm={20} md={16} lg={12}>
-        <Form>
-          <Form.Item label="Username">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Password">
-            <Input.Password />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
+    <>
+      <Row
+        justify="space-evenly"
+        align={"middle"}
+        style={{ background: "grey" }}
+      >
+        <Col xs={24} md={{ span: 12, order: 4 }} lg={{ span: 6, order: 1 }}>
+          <div
+            style={{ height: "200px", width: "200px", backgroundColor: "red" }}
+          />
+        </Col>
+        <Col xs={24} md={{ span: 12, order: 3 }} lg={{ span: 6, order: 2 }}>
+          <div
+            style={{ height: "200px", width: "200px", backgroundColor: "blue" }}
+          />
+        </Col>
+        <Col xs={24} md={{ span: 12, order: 2 }} lg={{ span: 6, order: 3 }}>
+          <div
+            style={{
+              height: "200px",
+              width: "200px",
+              backgroundColor: "green",
+            }}
+          />
+        </Col>
+        <Col xs={24} md={{ span: 12, order: 1 }} lg={{ span: 6, order: 4 }}>
+          <div
+            style={{
+              height: "200px",
+              width: "200px",
+              backgroundColor: "purple",
+            }}
+          />
+        </Col>
+      </Row>
+      <Row style={{ background: "pink" }} justify="space-evenly">
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+      </Row>
+      <Row style={{ background: "pink" }} justify="center">
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+      </Row>
+      <Row style={{ background: "pink" }} justify="end">
+        <Col span={4} style={{ textAlign: "center" }}>
+          col-4
+        </Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+      </Row>
+      <Row style={{ background: "pink" }} justify="space-between">
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+        <Col span={4}>col-4</Col>
+      </Row>
+    </>
   );
 };
 
