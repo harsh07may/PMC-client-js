0aa65a16138506fc48e740214378917e644caa1a
Nevan Alphonso <nevanalphonso@gmail.com>
2023-07-19 15:20:08 +0530

    testing antd Collapse (doesn't work)

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,6 +1,30 @@
 import axios from "axios";
 import React from "react";
 import { getEnv } from "../utils/getEnv";
+import { Collapse } from "antd";
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+const items = [
+  {
+    key: "1",
+    label: "This is panel header 1",
+    children: <p>{text}</p>,
+  },
+  {
+    key: "2",
+    label: "This is panel header 2",
+    children: <p>{text}</p>,
+  },
+  {
+    key: "3",
+    label: "This is panel header 3",
+    children: <p>{text}</p>,
+  },
+];
 
 const TestPage = () => {
   async function callAPI() {
@@ -16,6 +40,7 @@ const TestPage = () => {
       <p>Test Page</p>
       <button onClick={() => console.log("clicked")}>clg</button>
       <button onClick={callAPI}>api</button>
+      <Collapse items={items} defaultActiveKey={["1"]} />
     </div>
   );
 };
